b22cd8e677e789c2f23541882052a151abfe969e
Jan Helge Aalbu <jhaalbu@gmail.com>
2022-12-17 16:32:09 +0100

    arbeid med sonegeometri

--- sonegeometri.py
+++ sonegeometri.py
@@ -14,34 +14,22 @@ ro = math.atan(tan_ro)
 r = 0.5
 
 def sonegeometri(b0, qv, qh, ro, r):
-    
-    f_omega = (1-(math.sqrt(1-r**2)))
-    print(f'f_omega:{f_omega}')
-    omega = math.atan(f_omega)
-    print(f'omega:{omega}')
+    tan_omega = ((1-(math.sqrt(1-r**2)))/r)*((math.tan(math.radians(45 + (math.degrees(ro)/2)))))
+    omega = math.atan(tan_omega)
     tan_alfa = math.tan(math.radians(45) + ro/2)
-    print(f'tan_alfa:{tan_alfa}')
-    tan_omega = f_omega * tan_alfa
-    print(f'tan_omega:{tan_omega}')
     beta1 = math.radians(45) + ro/2 - omega
-    print(f'beta1:{beta1}')
     beta2 = math.radians(45) + ro/2 + omega
-    print(f'beta2:{beta2}')
     beta3 = math.radians(90) - ro
-    print(f'beta3:{beta3}')
     beta4 = math.radians(45) - ro/2
-    print(f'beta4:{beta4}')
-    phi = math.radians(90) - omega
-    print(f'phi:{phi}')
-    r1 = b0 * (math.sin(beta1)/math.sin(beta2))
-    print(f'r1:{r1}')
-    r2 = r1 * math.exp(phi * math.tan(ro))
-    print(f'r2:{r2}')
+    theta = math.radians(90) - omega
+    r1 = b0 * (math.sin(beta1))/(math.sin(beta3))
+    r2 = r1 * math.exp(theta * math.tan(ro))
 
-    return beta1, beta2, beta3, beta4, phi, r1, r2
+    return beta1, beta2, beta3, beta4, theta, r1, r2
 
 geometri = sonegeometri(b0, qv, qh, ro, r)
-print(geometri)
+
+def tegn_fundament(b0, )
 
 for i in geometri:
     print(math.degrees(i))
