93a9fc898dc9209c2e0379c3905d540efa00a6af
Jan Helge Aalbu <31095620+jhaalbu@users.noreply.github.com>
2024-11-01 10:19:50 +0100

    Update ⛰️ Terrengprofil.py

--- pages/⛰️ Terrengprofil.py
+++ pages/⛰️ Terrengprofil.py
@@ -320,7 +320,7 @@ def alfabeta(df_heile, losne):
 def csv_bearbeiding(fil):
     '''Fikser fil med høydatada.no som levere både DTM og DOM'''
     df = pd.read_csv(uploaded_file, sep=';', skiprows=1)
-    df = df.loc[: df[(df['X'] == 'Source: DOM1')].index[0] - 1, :]
+    #df = df.loc[: df[(df['X'] == 'Source: DOM1')].index[0] - 1, :]
     df = df.astype('float64')
     return df
 
